aca3a721f725f7b1fefa5e4676f290f78548011f
whyang115 <whyang115@gmail.com>
2020-08-17 15:24:12 +0800

    feat: init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://www.mingdao.com/api/',
+  headers: { 'Access-Control-Allow-Origin': 'all' },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    instance.get('Oauth2/genImageCaptcha').then(res => {
+      console.log(res);
+    });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
