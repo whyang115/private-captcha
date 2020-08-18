c30a80d026f9874f511add5e9efb664c9e168043
whyang115 <whyang115@gmail.com>
2020-08-18 10:57:01 +0800

    feat: rebuild

---
+++ build/precache-manifest.4063653fff51a935d4da21d452e3854d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b13793975869a713350f943f51712f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a58a9375204a36d0f349",
+    "url": "./static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "23a77b5aca99d019ba10",
+    "url": "./static/css/main.972fd717.chunk.css"
+  },
+  {
+    "revision": "a58a9375204a36d0f349",
+    "url": "./static/js/2.986a0a96.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.986a0a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a77b5aca99d019ba10",
+    "url": "./static/js/main.88745ae7.chunk.js"
+  },
+  {
+    "revision": "794ffcc1f51c13aa0826",
+    "url": "./static/js/runtime-main.d203399f.js"
+  }
+]);
